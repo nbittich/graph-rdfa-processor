8b966f726f50120fada3cb5e78cc206ffc0ebf3a
Nordine Bittich <contact@bittich.be>
2024-12-08 11:35:30 +0100

    gh page build

---
+++ rdfa-wasm/app.js
@@ -0,0 +1,80 @@
+import init, { html_to_rdfa } from "./pkg/rdfa_wasm.js";
+
+async function run() {
+  const form = document.querySelector("form");
+  form.classList.add("d-none");
+  await init();
+  form.classList.remove("d-none");
+
+  const text_area = document.querySelector("#html");
+  text_area.value = `
+            <!DOCTYPE html>
+            <html prefix="foaf: http://xmlns.com/foaf/0.1/">
+              <head>
+              <title>Test 0083</title>
+              </head>
+              <body>
+             <div about="http://www.example.org/#somebody" rel="foaf:knows">
+                 <p property="foaf:name">Ivan Herman</p>
+              <p rel="foaf:mailbox" resource="mailto:ivan@w3.org">mailto:ivan@w3.org</p>
+              <p about="http://danbri.org/foaf.rdf#danbri" typeof="foaf:Person" property="foaf:name">Dan Brickley</p>
+             </div>
+              </body>
+            </html>
+        `;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    let res = html_to_rdfa(
+      data.get("html") || "",
+      data.get("base") || "",
+      data.get("wellKnownPrefix") || "",
+    );
+    const out = document.querySelector("pre");
+    out.innerText = res;
+  });
+  const issueLink = document.querySelector("#issueLink");
+  issueLink.onclick = (e) => {
+    e.preventDefault();
+    const a = document.createElement("a");
+    let params = new URLSearchParams();
+    const data = new FormData(form);
+    params.append("title", "RDFa processing bug");
+
+    params.append(
+      "body",
+      `
+### Base: 
+  
+  \`${data.get("base") || ""}\`
+
+### Well Known Prefix:
+
+  \`${data.get("base") || ""}\`
+
+### Html:
+  
+   \`\`\`html
+   ${data.get("html") || ""}
+   \`\`\`
+
+          `,
+    );
+    a.href = `https://github.com/nbittich/graph-rdfa-processor/issues/new?${params.toString()}`;
+    a.target = "_blank";
+    a.click();
+  };
+}
+const copyToClipboardBtn = document.querySelector("#copyToClipboard");
+copyToClipboardBtn.onclick = function (e) {
+  e.preventDefault();
+  const out = document.querySelector("pre");
+
+  navigator.clipboard.writeText(out.innerText);
+
+  alert("Copied to clipboard");
+};
+
+run();
+
