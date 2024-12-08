db8a6359db92b7ce176da20e833ed5848d1e63e9
Nordine Bittich <contact@bittich.be>
2024-12-08 14:41:21 +0100

    use lol_alloc

--- rdfa-wasm/src/lib.rs
+++ rdfa-wasm/src/lib.rs
@@ -1,8 +1,12 @@
 #![cfg(target_arch = "wasm32")]
 mod utils;
 use graph_rdfa_processor::RdfaGraph;
+use lol_alloc::{AssumeSingleThreaded, FreeListAllocator};
 use wasm_bindgen::prelude::*;
-
+// SAFETY: This application is single threaded, so using AssumeSingleThreaded is allowed.
+#[global_allocator]
+static ALLOCATOR: AssumeSingleThreaded<FreeListAllocator> =
+    unsafe { AssumeSingleThreaded::new(FreeListAllocator::new()) };
 #[wasm_bindgen]
 pub fn html_to_rdfa(html: &str, base: &str, well_known_prefix: &str) -> String {
     utils::set_panic_hook();
